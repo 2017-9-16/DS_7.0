04921087d4816a55c35a75e4c1d151380cecc767
陈翰 <chenhan199781@gmail.com>
2018-06-07 19:54:36 +0800

    FINAL

--- DS_7.0/main.cpp
+++ DS_7.0/main.cpp
@@ -5,7 +5,6 @@
 //  Created by Walter Mitty on 2018/6/5.
 //  Copyright © 2018年 Walter Mitty. All rights reserved.
 //
-
 #include <stdio.h>
 #include <stdlib.h>
 #define MaxNodeNum 20
@@ -18,34 +17,35 @@ typedef struct vnode{ //顶点表结点
 VertexType vertex; //顶点域
 EdgeNode *firstedge; // 边表头指针
 } VertexNode;
-//typedef VertexNode AdjList[MaxNodeNum];
+typedef VertexNode AdjList[MaxNodeNum];
 typedef struct{
-    //AdjList adjlist;// 邻接表
-    VertexNode adjlist[MaxNodeNum];
+    AdjList adjlist;// 邻接表
     int n,e; //顶点数和边数
-}ALGraph; //对于简单应用无需定义此类型，直接使用AdjList
+}ALGraph;
+
 void  CreateGraph(ALGraph* G) {
     int i,j,k;
     EdgeNode *s;
-    //G=(ALGraph*)malloc(sizeof(ALGraph));
-    
-    printf("输入顶点数和边数\n");
+    int m;
+    printf("要建立的是有向图（1）还是无向图（0），请选择（输入1或0）：\n");
+    scanf("%d",&m);
+    printf("请输入图的顶点数\n");
     scanf("%d",&(G->n)); //读入顶点数和边数
+    printf("请输入图的边数\n");
     scanf("%d",&(G->e));
     //getchar();
+    printf("请输入图的各顶点信息：\n");
     for(i=0;i<G->n;i++){ //建立顶点表
         
-            printf("请输入第%d个顶点\n",i+1);
+            printf("第%d个顶点信息",i+1);
             getchar();
             scanf("%c",&(G->adjlist[i].vertex));
             G->adjlist[i].firstedge=NULL;
         
     } //边表置空
-    int m;
-    printf("请输入1创建有向图，0创建无向图\n");
-    scanf("%d",&m);
+
     for(k=0;k<G->e;k++){ //建立边表
-        printf("请输入第%d条边\n",k+1);
+        printf("请输入第%d条边(以空格分隔)",k);
         scanf("%d%d",&i,&j); //读入边(vi,vj)的顶点对序
         s=(EdgeNode *)malloc(sizeof(EdgeNode)); //生成边表结点
         s->adjvex=j; //邻结点的序号为j
@@ -58,7 +58,6 @@ void  CreateGraph(ALGraph* G) {
             s->next=G->adjlist[j].firstedge;
             G->adjlist[j].firstedge=s; //将新结点*s插入vj头部
         }
-       
 }
  
     
@@ -77,17 +76,12 @@ void print(ALGraph *G)
             p=p->next;
         }
         printf("\n");
-        
-        
-        
     }
 }
 
 int main() {
+    
     ALGraph G ;
     CreateGraph(&G);
     print(&G);
-    
-    
-   // printf("select 0for alGraph and  for graph\n");
 }
